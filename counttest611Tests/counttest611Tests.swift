f444b57d50a1f730c0d755dc943ed731edad8521
odakayaka <ayaka.odaka@gmail.com>
2016-06-11 15:47:08 +0900

    Initial Commit

---
+++ counttest611Tests/counttest611Tests.swift
@@ -0,0 +1,36 @@
+//
+//  counttest611Tests.swift
+//  counttest611Tests
+//
+//  Created by 尾高文香 on 2016/06/11.
+//  Copyright © 2016年 com.odakaayaka. All rights reserved.
+//
+
+import XCTest
+@testable import counttest611
+
+class counttest611Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
